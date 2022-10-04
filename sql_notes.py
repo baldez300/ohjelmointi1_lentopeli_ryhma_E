26124e41e3003d1f00c41cd07204255e8a02954b
demonhearted <violetnetwork@pm.me>
2022-10-04 11:10:06 +0300

    edited

--- sql_notes.py
+++ sql_notes.py
@@ -1,5 +1,12 @@
+# at the start of the game saves username and starting location
+sql = f"INSERT INTO game (screen_name)" \
+      "VALUES({username})" \
+    "INSERT INTO game (location)" \
+    "VALUES " # player's starting location
+
 # every time player moves to another airport
-INSERT INTO game TABLE
+sql = "INSERT INTO game (location)" \
+    "VALUES" #player's current location
 
 # at the end
 # laskee nykyisen käyttäjän pisteet ja näyttää ne
@@ -8,3 +15,4 @@ INSERT INTO game TABLE
 # top 5
 # laskutoimitus pisteet kaikille käyttäjille
 # näyttää 5 korkeinta pistemäärää
+sql = "ORDER BY points_amount DESC"
