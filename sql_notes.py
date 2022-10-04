0b4e56a841633c6e8102526a7d2112ff9b41968e
demonhearted <violetnetwork@pm.me>
2022-10-04 11:06:12 +0300

    added

---
+++ sql_notes.py
@@ -0,0 +1,10 @@
+# every time player moves to another airport
+INSERT INTO game TABLE
+
+# at the end
+# laskee nykyisen käyttäjän pisteet ja näyttää ne
+# co2.game & location.game
+
+# top 5
+# laskutoimitus pisteet kaikille käyttäjille
+# näyttää 5 korkeinta pistemäärää
