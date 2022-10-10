ba9af8fc4f8b16929aec2addc9eeb86b7063750d
demonhearted <violetnetwork@pm.me>
2022-10-10 12:39:08 +0300

    top5 added

--- sql_notes.py
+++ sql_notes.py
@@ -42,22 +42,11 @@ print(check_tickets_continents())
 # ---------------------------
 
 # top 5
-# hakee tietokannasta käyttäjät jotka ovat keränneet 10 lippua ja käyneet 6 maanosassa
-# https://stackoverflow.com/questions/28755505/how-to-convert-sql-query-results-into-a-python-dictionary
-# https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursordict.html
-# https://www.tutorialspoint.com/how-to-sort-a-dictionary-in-python
-# https://pynative.com/python-cursor-fetchall-fetchmany-fetchone-to-read-rows-from-table/
-# TODO: test if works; maybe sort with sql code and fetchmany(5) instead of fetchall?
 def score():
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT screen_name, co2_consumed FROM game WHERE tickets_amount = '10' and continents_amount ='6'")
+    cursor = connection.cursor()
+    cursor.execute("SELECT screen_name, co2_consumed FROM game WHERE tickets_amount = '10' and continents_amount ='6' ORDER BY co2_consumed ASC LIMIT 5")
     result = cursor.fetchall()
     return result
 
-# TODO: order by desc, limit print to top 5, idk if this shit works yet
-# creates another dictionary that sorts by value
-score2 = dict(sorted(score.items(), key=lambda x:x[1]))
-# prints top5
-print("TOP 5:")
-for s in range(5):
-    print("* {name}".format(Name=row['Name']
+
+print(score())
