d66bee1493a05ef063e2826e247092c59cf9d9cc
demonhearted <111851035+demonhearted@users.noreply.github.com>
2022-10-11 11:11:29 +0000

    Update test.py

--- test.py
+++ test.py
@@ -12,22 +12,3 @@ def connect_database():
 
 
 connection = connect_database()
-
-def score():
-    cursor = connection.cursor()
-    cursor.execute("SELECT screen_name, co2_consumed FROM game WHERE tickets_amount = '10' and continents_amount ='6' ORDER BY co2_consumed ASC LIMIT 5;")
-    result = cursor.fetchall()
-    return result
-
-
-print(score())
-
-# creates another dictionary that sorts by value
-#score2 = dict(sorted(score.items(), key=lambda x:x[1]))
-# prints top5
-#print("TOP 5:")
-#for s in range(5):
-#    print("* {name}".format(Name=row['Name']
-
-
-# -----------------------------------------------------------------
