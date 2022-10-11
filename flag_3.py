01bff57c9e671d2a7658f7a9f7c2e4ef0127668c
baldez300 <81166801+baldez300@users.noreply.github.com>
2022-10-11 10:32:12 +0300

    Add files via upload

---
+++ flag_3.py
@@ -0,0 +1,37 @@
+﻿# Count visited countries flags
+
+from collections import Counter
+import mysql.connector
+import flag
+
+
+def get_countries_code(code_input):
+    sql = f"SELECT iso_country FROM country WHERE country.name = '{code_input}';"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()  # type of result: list
+
+    flag_list = {}
+    for code in result:
+        codestr = ''.join(code)
+        lippu = flag.flag(codestr)
+        print(lippu)
+        flag_list[code] = lippu
+    return flag_list
+
+
+def connect_database():
+    return mysql.connector.connect(
+         host='127.0.0.1',
+         port=3306,
+         database='flight_game',
+        user='.....',
+        password='....',
+        autocommit=True
+    )
+
+
+connection = connect_database()
+
+flag_input = input("Anna country full name: ")
+get_countries_code(flag_input)
