beac1303318786f008895d4f5b432ef57083aa82
demonhearted <111851035+demonhearted@users.noreply.github.com>
2022-10-11 10:43:15 +0000

    Update sql_notes.py

--- sql_notes.py
+++ sql_notes.py
@@ -10,16 +10,17 @@ def connect_database():
         autocommit=True
          )
 
+
 connection = connect_database()
 
 # saves username - checked and works
 sql = f"INSERT INTO game (screen_name) VALUES ('{username}')"
 
 # saves starting location - checked and works
-sql = f"UPDATE game SET location = ('{start_airport}') WHERE screen_name = '{username}'"
+sql = f"UPDATE game SET location = 'FI' WHERE screen_name = '{username}'"
 
 # every time player moves to another airport - checked and works
-sql = f"UPDATE game SET location = ('{airportIcaoCode}') WHERE screen_name = '{username}'"
+sql = f"UPDATE game SET location = ('{isocountry}') WHERE screen_name = '{username}'"
 # every time player moves to another country - checked and works
 sql = f"UPDATE game SET tickets_amount = tickets_amount + 1 WHERE screen_name = '{username}'"
 # if player moves to another continent - checked and works
@@ -33,3 +34,21 @@ sql = f"UPDATE game SET co2_consumed = ('{co2_overall_used}') WHERE screen_name 
 
 # top 5, gets 5 users with lowest co2 used and orders by ascending - checked and works
 sql = "SELECT screen_name, co2_consumed FROM game WHERE tickets_amount = '10' and continents_amount ='6' ORDER BY co2_consumed ASC LIMIT 5"
+cursor = connection.cursor()
+cursor.execute(sql)
+result = cursor.fetchall()
+
+# checks if player is in t5 - checked and works
+sql = "SELECT screen_name, co2_consumed FROM game WHERE tickets_amount = '10' and continents_amount ='6' ORDER BY co2_consumed ASC LIMIT 5"
+cursor = connection.cursor()
+cursor.execute(sql)
+result = cursor.fetchall()
+names = []
+for name in result:
+    names.append(name[0])
+print(names)
+
+if username in names:
+    print("olet t5")
+else:
+    print("et ole t5")
